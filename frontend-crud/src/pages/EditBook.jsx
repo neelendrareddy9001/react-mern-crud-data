b96970e82148afcef5563b76879a61833b79eb03
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-09-22 22:09:51 +0200

    updated editbook ui

--- frontend-crud/src/pages/EditBook.jsx
+++ frontend-crud/src/pages/EditBook.jsx
@@ -25,7 +25,7 @@ const EditBook = () => {
       console.log(error)
     })
   },[])
-  const handleEditBook = () => {
+  const handleSaveBook = () => {
     const data = {
       title, 
       author,
@@ -45,7 +45,35 @@ const EditBook = () => {
       })
   }
   return (
-    <div>EditBook</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Edit Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto my-8'>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
+          <input 
+            type='text'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full rounded-md' 
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
+          <input type='text' value={author} onChange={(e) => setAuthor(e.target.value)}
+              className='border-2 border-gray-500 px-4 py-2 w-full rounded-md'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
+          <input type='text' value={author} onChange={(e) => setAuthor(e.target.value)}
+              className='border-2 border-gray-500 px-4 py-2 w-full rounded-md'
+          />
+        </div>
+        <button className='p-2 bg-sky-300 m-8 rounded-xl' onClick={handleSaveBook}>Save</button>
+      </div>
+    </div>
   )
 }
 
