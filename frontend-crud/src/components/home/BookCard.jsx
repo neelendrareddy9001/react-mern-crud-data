9e7553f86ec58c319ba2c6d5650de2ce4a8fcf04
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-09-25 12:13:22 +0200

    updated backend data as table or card type component defined

---
+++ frontend-crud/src/components/home/BookCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Link } from 'react-router-dom';
+import {AiOutlineEdit} from 'react-icons/ai'
+import {BsInfoCircle} from 'react-icons/bs';
+import {MdOutlineAddBox, MdOutlineDelete} from 'react-icons/md';
+
+const BookCard = ({books}) => {
+  return (
+   <h1>Card</h1>
+  );
+};
+
+export default BookCard;
