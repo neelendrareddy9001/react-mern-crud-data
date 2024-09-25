4756f3136bc54bebcad4a5b8933324519059d7b7
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-09-25 12:35:57 +0200

    made single each and every book as a individual book by creating singleCard

--- frontend-crud/src/components/home/BookCard.jsx
+++ frontend-crud/src/components/home/BookCard.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
-import { Link } from 'react-router-dom';
-import {AiOutlineEdit} from 'react-icons/ai'
-import {BsInfoCircle} from 'react-icons/bs';
-import {MdOutlineAddBox, MdOutlineDelete} from 'react-icons/md';
+import { Link } from "react-router-dom";
 
-const BookCard = ({books}) => {
+import BookSinglCard from "./BookSinglCard";
+
+const BookCard = ({ books }) => {
   return (
-   <h1>Card</h1>
+    <>
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {books.map((item) => (
+          <BookSinglCard key={item._id} book={item} />
+        ))}
+      </div>
+    </>
   );
 };
 
